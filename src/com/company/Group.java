00eede723324cc267733b8dd6f22d98e6c75fcd6
ibragimovic <m.ibragim88@gmail.com>
2020-11-29 21:56:23 +0600

    Task B

--- src/com/company/Group.java
+++ src/com/company/Group.java
@@ -1,7 +1,41 @@
 package com.company;
 
+import java.util.Date;
+
 public class Group {
-    
+Student[] array;
+String course;
+Date begin;
+int lengthCourse;
 
 
+
+public void setArray(Student[] array) {
+        this.array = array;
+    }
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    public void setBegin(Date begin) {
+        this.begin = begin;
+    }
+    public void setLengthCourse(int lengthCourse) {
+        this.lengthCourse = lengthCourse;
+    }
+
+    public Student[] getArray() {
+        return array;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public Date getBegin() {
+        return begin;
+    }
+
+    public int getLengthCourse() {
+        return lengthCourse;
+    }
 }
