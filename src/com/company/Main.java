d23ce04cc8940e805bd6c0d48efe052a06628064
Ибрагим <m.ibragimm88@gmail.com>
2020-11-29 13:20:41 +0600

    creating home work branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Rectangle rect = new Rectangle();
+        Rectangle rect2 = new Rectangle();
+
+        rect.setHeight(10);
+        rect.setWidth(20);
+
+        rect2.setHeight(100);
+        rect2.setWidth(30);
+
+        System.out.println("Rect height = " + rect.getHeight() + ", rect width = " + rect.getWidth());
+        System.out.println("Rectangle area = " + rect.getArea());
+
+        System.out.println("Rect2 area = " + rect2.getArea());
+
+//        Rectangle square = new Rectangle();
+//        square.setHeight(15);
+//        square.setWidth(15);
+//        square.setColor("WHITE");
+//        System.out.println("Square area = " + square.getArea() +
+//                ", color is " + square.getColor() + ".");
+
+          Rectangle square = new Rectangle();
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        square.setWidth(size);
+        square.setHeight(size);
+        square.setColor("WHITE");
+        System.out.println("Square area = " + square.getArea() +
+                ", color is " + square.getColor() + ".");
+
+        Rectangle rect3 = new Rectangle();
+        System.out.println("Rect 3 height = " + rect3.getHeight());
     }
 }
---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Rectangle {
+    int height;
+    int width;
+    String color;
+
+    public void setHeight(int h){
+        if (h <= 0){
+            System.out.println("Height can't be less than 0 or 0.");
+            return;
+        }
+        height = h;
+    }
+
+    public void setWidth(int width) {
+        if (width <= 0){
+            System.out.println("Width can't be less than 0 or 0.");
+            return;}
+        this.width = width;
+    }
+
+        public void setColor(String color){
+        this.color = color;
+    }
+
+    public int getArea(){
+        return height * width;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public String getColor(){
+        return color;
+        }
+        }
+
